d2fa77b6741416832919ea16e788bf23db619c15
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2022-10-20 16:49:14 -0700

    fix adding tasks with different types to sublists

--- todoapp/src/firebase.js
+++ todoapp/src/firebase.js
@@ -1,17 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { arrayRemove, arrayUnion, deleteDoc, deleteField, doc, getFirestore, setDoc, updateDoc } from "firebase/firestore";
+import {
+	arrayRemove,
+	arrayUnion,
+	deleteDoc,
+	deleteField,
+	doc,
+	getFirestore,
+	setDoc,
+	updateDoc,
+} from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
 import { defaultTask, parseNewTextToUpdateObject } from "./misc/options";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCF7OvgCxXof4ZYWlPWDud9eHGy7byiO4k",
-    authDomain: "blockschedule-88b65.firebaseapp.com",
-    databaseURL: "https://blockschedule-88b65-default-rtdb.firebaseio.com",
-    projectId: "blockschedule-88b65",
-    storageBucket: "blockschedule-88b65.appspot.com",
-    messagingSenderId: "135479349433",
-    appId: "1:135479349433:web:3cb85d8c8706411da40b68",
+	apiKey: "AIzaSyCF7OvgCxXof4ZYWlPWDud9eHGy7byiO4k",
+	authDomain: "blockschedule-88b65.firebaseapp.com",
+	databaseURL: "https://blockschedule-88b65-default-rtdb.firebaseio.com",
+	projectId: "blockschedule-88b65",
+	storageBucket: "blockschedule-88b65.appspot.com",
+	messagingSenderId: "135479349433",
+	appId: "1:135479349433:web:3cb85d8c8706411da40b68",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,94 +30,94 @@ export const firestore = getFirestore(app);
 let user = null;
 
 onAuthStateChanged(auth, function (u) {
-    if (u) {
-        user = u;
-    }
+	if (u) {
+		user = u;
+	}
 });
 
 export function createTask(taskList, text) {
-    if (!text) return;
+	if (!text) return;
 
-    console.log("Creating task: " + text);
+	console.log("Creating task: " + text);
 
-    let taskUuid = uuidv4();
-    let updateObject = {
-        order: arrayUnion(taskUuid),
-    };
+	let taskUuid = uuidv4();
+	let updateObject = {
+		order: arrayUnion(taskUuid),
+	};
 
-    let taskUpdateObject = parseNewTextToUpdateObject(text);
-    // prevent deep nested tasks
-    if (taskList !== "tasks") {
-        taskUpdateObject.type = defaultTask.type;
-        taskUpdateObject.wasList = false; // todo wastes storage space
-    }
+	let taskUpdateObject = parseNewTextToUpdateObject(text);
+	// prevent deep nested tasks
+	if (taskList !== "tasks") {
+		// taskUpdateObject.type = defaultTask.type; // why tf was this here
+		taskUpdateObject.wasList = false; // todo wastes storage space
+	}
 
-    updateObject.order = arrayUnion(taskUuid);
-    console.log(taskUpdateObject);
-    console.log({ ...defaultTask, ...taskUpdateObject });
-    updateObject[taskUuid] = { ...defaultTask, ...taskUpdateObject };
+	updateObject.order = arrayUnion(taskUuid);
+	console.log(taskUpdateObject);
+	console.log({ ...defaultTask, ...taskUpdateObject });
+	updateObject[taskUuid] = { ...defaultTask, ...taskUpdateObject };
 
-    const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
-    setDoc(taskDocRef, updateObject, { merge: true });
+	const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
+	setDoc(taskDocRef, updateObject, { merge: true });
 }
 
 export function updateTask(taskList, taskId, newData) {
-    console.log("Updating task " + taskId);
+	console.log("Updating task " + taskId);
 
-    // newData is an object
+	// newData is an object
 
-    let updateObject = {};
+	let updateObject = {};
 
-    for (let key of Object.keys(newData)) {
-        console.log(key);
-        let updateObjectKey = taskId + "." + key;
-        let newDataPiece = newData[key];
-        console.log(newDataPiece);
-        updateObject[updateObjectKey] = newDataPiece;
-    }
+	for (let key of Object.keys(newData)) {
+		console.log(key);
+		let updateObjectKey = taskId + "." + key;
+		let newDataPiece = newData[key];
+		console.log(newDataPiece);
+		updateObject[updateObjectKey] = newDataPiece;
+	}
 
-    const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
-    updateDoc(taskDocRef, updateObject);
+	const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
+	updateDoc(taskDocRef, updateObject);
 }
 
 export function deleteTask(taskList, taskId, wasList) {
-    console.log("Deleting " + taskId);
+	console.log("Deleting " + taskId);
 
-    if (wasList) {
-        console.log("Deleting potential sub list");
-        // will be counted as a delete op even if the list was never populated
-        deleteDoc(doc(firestore, `users/${user.uid}/data`, taskId));
-    }
+	if (wasList) {
+		console.log("Deleting potential sub list");
+		// will be counted as a delete op even if the list was never populated
+		deleteDoc(doc(firestore, `users/${user.uid}/data`, taskId));
+	}
 
-    let updateObject = {
-        order: arrayRemove(taskId),
-    };
-    updateObject[taskId] = deleteField();
+	let updateObject = {
+		order: arrayRemove(taskId),
+	};
+	updateObject[taskId] = deleteField();
 
-    const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
-    updateDoc(taskDocRef, updateObject);
+	const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
+	updateDoc(taskDocRef, updateObject);
 }
 
 export function updateTaskOrder(taskList, newOrder) {
-    console.log("Rearranging", newOrder);
-    let updateObject = {
-        order: newOrder,
-    };
-    const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
-    updateDoc(taskDocRef, updateObject);
+	console.log("Rearranging", newOrder);
+	let updateObject = {
+		order: newOrder,
+	};
+	const taskDocRef = doc(firestore, `users/${user.uid}/data`, taskList);
+	updateDoc(taskDocRef, updateObject);
 }
 
 export function updateConfigColors(word, color) {
-    console.log("Saving colors");
-    let updateObject = {
-        [word]: color,
-    };
+	console.log("Saving colors");
+	let updateObject = {
+		[word]: color,
+	};
 
-    if (color.toLowerCase() === "#ffffff") {
-        updateObject[word] = deleteField();
-    }
-    const configDocRef = doc(firestore, `users/${user.uid}/data`, "config");
-    setDoc(configDocRef, updateObject, { merge: true });
+	if (color.toLowerCase() === "#ffffff") {
+		updateObject[word] = deleteField();
+	}
+	const configDocRef = doc(firestore, `users/${user.uid}/data`, "config");
+	setDoc(configDocRef, updateObject, { merge: true });
 }
 
 export function cleanUnusedListDocs(mainTaskIdList) {}
--- todoapp/src/misc/options.js
+++ todoapp/src/misc/options.js
@@ -1,7 +1,7 @@
 import { deleteField } from "firebase/firestore";
 import isUrl from "is-url";
 
-export const versionNumber = 1.9;
+export const versionNumber = "1.10";
 
 export const typeOptions = ["long", "short", "email", "list", "thought"]; // order controls sorted order
 export const typeOptionsDisplay = { thought: "💡", email: "📧" };
